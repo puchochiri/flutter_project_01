c10e52bd441795ac07c80f24cbf6e9bc1fcce6fc
puchochiri <doawishfor@naver.com>
2023-08-01 22:21:23 +0900

    commit

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,46 @@
 import 'package:flutter/material.dart';
 
-class homeScreen extends s
+class homeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+          //  반대축에서 이동할 공간을 제공하기 위해 높이를 최대한으로 설정
+          height: double.infinity,
+          child:  Row(
+            // 주축 정렬
+            mainAxisAlignment: MainAxisAlignment.center,
+            // 반대출정렬
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              TopTitle(),
+              TopNav(),
+              BoardList(),
+
+            ],
+
+          ),
+        ),
+
+
+      ),
+    )
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
+
+class TopTitle {
+
+}
+
+class TopNav {
+
+}
+
+class BoardList {
+
+}
